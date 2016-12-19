b89eee355d10dab0d543bb3b838ea17a03056d7c
goks <gokulav2@gmail.com>
2016-12-19 16:19:47 +0530

    First commit

---
+++ tryla.py
@@ -0,0 +1,12 @@
+from PIL import Image, ImageCms
+
+im = Image.open('col.jpg')
+if im.mode != "RGB":
+  im = im.convert("RGB")
+
+srgb_profile = ImageCms.createProfile("sRGB")
+lab_profile  = ImageCms.createProfile("LAB")
+
+rgb2lab_transform = ImageCms.buildTransformFromOpenProfiles(srgb_profile, lab_profile, "RGB", "LAB")
+lab_im = ImageCms.applyTransform(im, rgb2lab_transform)
+lab_im.save('worker.tiff')
